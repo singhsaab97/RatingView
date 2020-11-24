be4dbaedaa5154d8dc336b6de78bdf5304b5723e
Abhijit Singh <abhijit@noonacademy.com>
2020-11-25 00:35:23 +0530

    Initial project setup

---
+++ RatingView/RatingView.h
@@ -0,0 +1,18 @@
+//
+//  RatingView.h
+//  RatingView
+//
+//  Created by Abhijit Singh on 25/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RatingView.
+FOUNDATION_EXPORT double RatingViewVersionNumber;
+
+//! Project version string for RatingView.
+FOUNDATION_EXPORT const unsigned char RatingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RatingView/PublicHeader.h>
+
+
